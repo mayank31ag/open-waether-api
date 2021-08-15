b1e3fa65315f19ec5eb839f4845ced2b36364a1f
mayank3141@gmail.com <mayank3141@gmail.com>
2021-08-15 16:42:45 +0530

    change in api url

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import WeatherCard from "./WeatherCard";
 import { parseWeatherApiData } from "../utils/utils";
 const fetchUrl =
-  "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&units=metric";
+  "https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&units=metric";
 const Card = () => {
   const [data, setData] = useState(null);
   const getData = () => fetch(fetchUrl).then((res) => res.json());
